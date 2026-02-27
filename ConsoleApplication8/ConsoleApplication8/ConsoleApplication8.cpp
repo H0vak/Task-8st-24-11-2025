683da761495913acedb91cebe0de861cda9e1a9f
H0vak <novak.anton2k8@gmail.com>
2026-02-27 21:01:20 +0200

    Add files via upload

---
+++ ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    long long n, reversed = 0;
+    cout << "Введіть натуральне (мінімум двух значне) число: ";
+    cin >> n;
+
+    while (n > 0) {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+
+    cout << "Число у зворотному порядку: " << reversed;
+
+    return 0;
+}
